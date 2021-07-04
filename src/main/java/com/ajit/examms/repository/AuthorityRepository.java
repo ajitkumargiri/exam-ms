5ebd7642e940e709caf2371f75c1d9c0bc459cc9
ajitgiri <ajit.bapina@gmail.com>
2021-07-04 19:55:19 +0530

    Initial version of examms generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/ajit/examms/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ajit.examms.repository;
+
+import com.ajit.examms.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
